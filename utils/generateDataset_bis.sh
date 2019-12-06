a7a1779b02f3ac31c503b4e1c2a444750676af5c
Paul Geneau <paul.geneau-de-lamarliere@ens-lyon.fr>
2019-12-06 13:53:57 +0100

    Added other versions of generateDataset.sh and sampleData.py

---
+++ utils/generateDataset_bis.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+SPLITSIZE="512K"
+COMPRESSION_RATIO=7
+
+DATA_PATH="./../pen-dataset/original"
+UNZIP="./../pen-dataset/unzip"
+SPLIT="./../pen-dataset/split"
+ZIP="./../pen-dataset/zip_$SPLITSIZE_$COMPRESSION_RATIO"
+
+# UNZIP ORIGINAL DATA
+gunzip -k "$DATA_PATH"/*
+mv "$DATA_PATH"/*.txt $UNZIP
+
+# RENAME
+for entry in "$UNZIP"/*
+do
+    NOEXTENSION=${entry%.txt}
+    mv $entry $NOEXTENSION
+done
+mv "$UNZIP"/* $SPLIT
+# cp "$UNZIP"/* $SPLIT
+
+# SPLIT
+for entry in "$SPLIT"/*
+do
+    split -b $SPLITSIZE "$entry" "$entry"_
+    rm $entry
+done
+mv "$SPLIT"/* $ZIP
+# /bin/cp "$SPLIT"/* $ZIP
+
+# ZIP
+for entry in "$ZIP"/*
+do
+    zip "-$COMPRESSION_RATIO" "$entry.zip" "$entry"
+    rm $entry
+done
